af57ee8f378edd47ae3e536b9f29d89d000ac185
Josué Hoenicka <josuee.vzla@gmail.com>
2022-11-25 16:22:53 -0300

    updates 25/11/22

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -9,12 +9,16 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
 export class MainComponent implements OnInit {
   name:any;
   main:any;
+  lang:any;
+  tech:any;
   constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
     this.check.test().subscribe(data => {
       this.name=data;
       this.main=data.education;
+      this.lang=data.languages;
+      this.tech=data.techs;
     });
   }
 
