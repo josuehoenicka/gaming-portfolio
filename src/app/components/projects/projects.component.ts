cd08c434d8f4116f8df4b1d6361cf76fdf38fa40
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 14:29:24 -0300

    update: migration projects section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+export class ProjectsComponent implements OnInit {
+  name:any;
+  experience:any;
+  certification:any;
+  lang:any;
+  tech:any;
+  project:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.experience=data.experience;
+      this.certification=data.education;
+      this.lang=data.languages;
+      this.tech=data.techs;
+      this.project=data.projects;
+    });
   }
 
 }
