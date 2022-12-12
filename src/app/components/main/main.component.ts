134884a0259ab8bb32c06c6574cfb2a5f397ea5c
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 14:42:20 -0300

    update: migration social

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,29 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
-  name:any;
-  experience:any;
-  certification:any;
-  lang:any;
-  tech:any;
-  project:any;
-  constructor(private check:PortfolioService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.check.test().subscribe(data => {
-      this.name=data;
-      this.experience=data.experience;
-      this.certification=data.education;
-      this.lang=data.languages;
-      this.tech=data.techs;
-      this.project=data.projects;
-    });
   }
 
 }
--- src/app/components/social/social.component.ts
+++ src/app/components/social/social.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.css']
 })
-export class SocialComponent implements OnInit {
 
-  constructor() { }
+export class SocialComponent implements OnInit {
+  name:any;
+  social:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.social=data.projects;
+    });
   }
 
 }
