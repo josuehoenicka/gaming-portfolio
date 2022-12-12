66da606be99c0163e23c2c969c9795e686953c7f
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 16:52:04 -0300

    create: routerlink to configure

--- src/app/components/skills-user/skills-user.component.ts
+++ src/app/components/skills-user/skills-user.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-skills-user',
   templateUrl: './skills-user.component.html',
   styleUrls: ['./skills-user.component.css']
 })
-export class SkillsUserComponent implements OnInit {
 
-  constructor() { }
+export class SkillsUserComponent implements OnInit {
+  name:any;
+  lang:any;
+  tech:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.lang=data.languages;
+      this.tech=data.techs;
+    });
   }
 
 }
