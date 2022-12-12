4afff42ec11ccc1a17f47a930d35afe3d6c623ce
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 16:48:50 -0300

    create: routerlink

--- src/app/components/about-user/about-user.component.ts
+++ src/app/components/about-user/about-user.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-about-user',
   templateUrl: './about-user.component.html',
   styleUrls: ['./about-user.component.css']
 })
-export class AboutUserComponent implements OnInit {
 
-  constructor() { }
+export class AboutUserComponent implements OnInit {
+  name:any;
+  experience:any;
+  certification:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.experience=data.experience;
+      this.certification=data.education;
+    });
   }
-
 }
