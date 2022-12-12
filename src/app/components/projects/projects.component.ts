4a9c1eed8bee6d72b4b1d52d83458cc87184f9e9
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 14:45:27 -0300

    fix: class projects ngOnInit

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,20 +9,12 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
 
 export class ProjectsComponent implements OnInit {
   name:any;
-  experience:any;
-  certification:any;
-  lang:any;
-  tech:any;
   project:any;
   constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
     this.check.test().subscribe(data => {
       this.name=data;
-      this.experience=data.experience;
-      this.certification=data.education;
-      this.lang=data.languages;
-      this.tech=data.techs;
       this.project=data.projects;
     });
   }
