f035cd4196bc77b5ce0836ba2bb382061ab50671
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 16:58:28 -0300

    create: routerlink to configure

--- src/app/components/social-user/social-user.component.ts
+++ src/app/components/social-user/social-user.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-social-user',
   templateUrl: './social-user.component.html',
   styleUrls: ['./social-user.component.css']
 })
-export class SocialUserComponent implements OnInit {
 
-  constructor() { }
+export class SocialUserComponent implements OnInit {
+  name:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+    });
   }
 
 }
