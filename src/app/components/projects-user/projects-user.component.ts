f4cdd468c08eea1ac610774517a175db2e1e58b5
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 16:55:39 -0300

    create: routerlink to configure

--- src/app/components/projects-user/projects-user.component.ts
+++ src/app/components/projects-user/projects-user.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-projects-user',
   templateUrl: './projects-user.component.html',
   styleUrls: ['./projects-user.component.css']
 })
-export class ProjectsUserComponent implements OnInit {
 
-  constructor() { }
+export class ProjectsUserComponent implements OnInit {
+  name:any;
+  project:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.project=data.projects;
+    });
   }
 
 }
