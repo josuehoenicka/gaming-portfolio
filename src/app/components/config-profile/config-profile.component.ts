f8d210c3d6d1f5fd75b0a1c0bd2290bc48190f97
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-13 19:11:38 -0300

    create: config profile template

--- src/app/components/config-profile/config-profile.component.ts
+++ src/app/components/config-profile/config-profile.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-config-profile',
   templateUrl: './config-profile.component.html',
   styleUrls: ['./config-profile.component.css']
 })
-export class ConfigProfileComponent implements OnInit {
 
-  constructor() { }
+export class ConfigProfileComponent implements OnInit {
+  name:any;
+  experience:any;
+  certification:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.experience=data.experience;
+      this.certification=data.education;
+    });
   }
-
 }
