1eced20d7548564c18e3cbc9f02feb885ca8c147
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 22:35:24 -0300

    update: routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUserComponent } from './components/about-user/about-user.component';
+import { AboutComponent } from './components/about/about.component';
 import { ConfigProfileComponent } from './components/config-profile/config-profile.component';
 import { ConfigProjectsComponent } from './components/config-projects/config-projects.component';
 import { ConfigSkillsComponent } from './components/config-skills/config-skills.component';
@@ -7,7 +9,13 @@ import { ConfigSocialComponent } from './components/config-social/config-social.
 import { HomeUserComponent } from './components/home-user/home-user.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProjectsUserComponent } from './components/projects-user/projects-user.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { RegisterComponent } from './components/register/register.component';
+import { SkillsUserComponent } from './components/skills-user/skills-user.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { SocialUserComponent } from './components/social-user/social-user.component';
+import { SocialComponent } from './components/social/social.component';
 
 const routes: Routes = [
   {
@@ -25,6 +33,38 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeUserComponent
+  },
+  {
+    path: 'home/user/about',
+    component: AboutUserComponent
+  },
+  {
+    path: 'home/user/skills',
+    component: SkillsUserComponent
+  },
+  {
+    path: 'home/user/projects',
+    component: ProjectsUserComponent
+  },
+  {
+    path: 'home/user/social',
+    component: SocialUserComponent
+  },
+  {
+    path: 'home/about',
+    component: AboutComponent
+  },
+  {
+    path: 'home/skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'home/projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'home/social',
+    component: SocialComponent
   },
   {
     path: 'configure/profile',
