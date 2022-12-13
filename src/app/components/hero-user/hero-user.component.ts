a37cd02072850fccfc2a876efed9e7b443168747
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-12 22:42:45 -0300

    create: hero user section

--- src/app/components/hero-user/hero-user.component.ts
+++ src/app/components/hero-user/hero-user.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-hero-user',
   templateUrl: './hero-user.component.html',
   styleUrls: ['./hero-user.component.css']
 })
-export class HeroUserComponent implements OnInit {
 
-  constructor() { }
+export class HeroUserComponent implements OnInit {
+  portfolio:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      console.log(data);
+      this.portfolio=data;
+    });
   }
 
 }
