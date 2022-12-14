6a69f3e81cfb078b571e20b20858607a8dba6b67
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-14 18:29:14 -0300

    update: btn back

--- src/app/components/building/building.component.ts
+++ src/app/components/building/building.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-building',
   templateUrl: './building.component.html',
   styleUrls: ['./building.component.css']
 })
+
 export class BuildingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location)
+  {}
+
+  backClicked() {
+    this._location.back();
+  }
 
   ngOnInit(): void {
   }
