fcd3736eaeff85cca9d748122f3e7ac2b9dea79b
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-14 12:23:21 -0300

    test: create config skills template

--- src/app/components/config-skills/config-skills.component.ts
+++ src/app/components/config-skills/config-skills.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-config-skills',
   templateUrl: './config-skills.component.html',
   styleUrls: ['./config-skills.component.css']
 })
-export class ConfigSkillsComponent implements OnInit {
 
-  constructor() { }
+export class ConfigSkillsComponent implements OnInit {
+  name:any;
+  project:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.project=data.projects;
+    });
   }
 
 }
