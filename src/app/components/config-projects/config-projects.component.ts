e77fe2181f5818903fb61b0b6b15026fab1686ff
Josué Hoenicka <josuee.vzla@gmail.com>
2022-12-14 16:37:20 -0300

    update: configure projects template

--- src/app/components/config-projects/config-projects.component.ts
+++ src/app/components/config-projects/config-projects.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-config-projects',
   templateUrl: './config-projects.component.html',
   styleUrls: ['./config-projects.component.css']
 })
-export class ConfigProjectsComponent implements OnInit {
 
-  constructor() { }
+export class ConfigProjectsComponent implements OnInit {
+  name:any;
+  project:any;
+  constructor(private check:PortfolioService) { }
 
   ngOnInit(): void {
+    this.check.test().subscribe(data => {
+      this.name=data;
+      this.project=data.projects;
+    });
   }
 
 }
