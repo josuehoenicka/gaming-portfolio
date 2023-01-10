b6edfff0a03ef43a1f6b1933be629b5c6e5502eb
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-09 22:36:02 -0300

    update: :)

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { AboutComponent } from './components/sections/about/about.component';
+import { ExperienceComponent } from './components/sections/experience/experience.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
+import { EducationComponent } from './components/sections/education/education.component';
+import { ContactComponent } from './components/sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
