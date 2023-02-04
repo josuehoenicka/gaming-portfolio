46b45d80b3ced80f2b15efa01b1595d5f03e5d96
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-04 17:04:35 -0300

    update: fix projects

--- portfolio/src/app/main/pages/experience/experience.component.ts
+++ portfolio/src/app/main/pages/experience/experience.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ExperienceComponent {
+  projects = [
+    {
+      name: 'Bandicootfolio',
+      description: 'This project',
+      deploy: '', github: '',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Wanderlist',
+      description: 'CRUD Countries List',
+      deploy: 'fa-lock', github: 'fa-lock',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Springular',
+      description: 'CRUD/JWT Portfolio',
+      deploy: 'fa-lock', github: 'fa-lock',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Hoenixstore',
+      description: 'Ecommerce Store',
+      deploy: 'fa-lock', github: 'fa-lock',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Jonikingames',
+      description: 'Video Game Website',
+      deploy: 'fa-lock', github: 'fa-lock',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Codebash',
+      description: 'Ecommerce Store',
+      deploy: 'fa-lock', github: 'fa-lock',
+      link_deploy: '', link_repo: '',
+      img: '../../../../assets/items/building.png'
+    }
+  ]
 }
