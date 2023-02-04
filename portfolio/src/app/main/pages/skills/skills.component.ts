b227eb2a7834dbeb64349ba9b2e4f7456d2eefc4
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-04 16:33:26 -0300

    update: clean html code

--- portfolio/src/app/main/pages/skills/skills.component.ts
+++ portfolio/src/app/main/pages/skills/skills.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  techs = [
+    {
+      name: 'HTML',
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'CSS/SCSS',
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'TypeScript',
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'JavaScript',
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'Java',
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'Angular',
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'Node.js',
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'SpringBoot',
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'MongoDB',
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'MySQL',
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+    },
+    {
+      name: 'Git',
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+    }
+  ]
 
 }
