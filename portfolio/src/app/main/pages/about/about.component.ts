77bb94f4eb462161fcd8b90bb2758b46e96efc46
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-05 12:17:52 -0300

    update: code migration from html to ts

--- portfolio/src/app/main/pages/about/about.component.ts
+++ portfolio/src/app/main/pages/about/about.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AboutComponent {
+  profile = {
+    name: 'Josue Hoenicka',
+    description: 'Front End Developer',
   }
-
+  general = [
+    {
+      description: 'Age:',
+      answer: '21'
+    },
+    {
+      description: 'Born:',
+      answer: 'Venezuela'
+    },
+    {
+      description: 'Live:',
+      answer: 'Argentina'
+    }
+  ]
+  languages = [
+    {
+      description: 'Spanish:',
+      answer: 'Fluent'
+    },
+    {
+      description: 'Portuguese:',
+      answer: 'Fluent'
+    },
+    {
+      description: 'Live:',
+      answer: 'Advanced'
+    }
+  ]
 }
