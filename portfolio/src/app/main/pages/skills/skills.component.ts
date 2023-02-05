55376e4a629ab882278c0d954af79aa322ac9dbf
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-05 12:42:55 -0300

    update: code migration from css to scss

--- portfolio/src/app/main/pages/skills/skills.component.ts
+++ portfolio/src/app/main/pages/skills/skills.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
 
