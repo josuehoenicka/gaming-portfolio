b8f21258cf3a5f06557be3b06226484057a8b004
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-05 13:04:54 -0300

    tablet design

--- portfolio/src/app/main/pages/skills/skills.component.ts
+++ portfolio/src/app/main/pages/skills/skills.component.ts
@@ -10,47 +10,58 @@ export class SkillsComponent {
   techs = [
     {
       name: 'HTML',
-      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'CSS/SCSS',
-      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'TypeScript',
-      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'JavaScript',
-      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'Java',
-      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'Angular',
-      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'Node.js',
-      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'SpringBoot',
-      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'MongoDB',
-      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'disabled', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'MySQL',
-      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'disabled', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     },
     {
       name: 'Git',
-      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' }
+      time: { one: 'actived', two: 'actived', three: 'disabled', four: 'disabled', five: 'disabled' },
+      nomobile: ''
     }
   ]
 
