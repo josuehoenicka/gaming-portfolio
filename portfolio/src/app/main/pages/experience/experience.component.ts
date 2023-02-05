d08566ba6dc6dbd1e3a7e11da1b426ac823149b8
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-05 13:06:28 -0300

    update: code migration from css to scss

--- portfolio/src/app/main/pages/experience/experience.component.ts
+++ portfolio/src/app/main/pages/experience/experience.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
   projects = [
