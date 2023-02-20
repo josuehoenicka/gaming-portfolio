be5e08bf4781e8c38aa6cbd92d1f1eedde8a2e32
Josué Hoenicka <josuee.vzla@gmail.com>
2023-02-20 13:00:38 -0300

    update: fix projects

--- portfolio/src/app/main/pages/experience/experience.component.ts
+++ portfolio/src/app/main/pages/experience/experience.component.ts
@@ -8,16 +8,30 @@ import { Component } from '@angular/core';
 export class ExperienceComponent {
   projects = [
     {
+      name: 'Rottwames',
+      description: 'Video Game Website',
+      deploy: 'fa-ban', github: 'fa-github',
+      link_deploy: '', link_repo: 'https://github.com/josuehoenicka/rottwames',
+      img: '../../../../assets/items/building.png'
+    },
+    {
+      name: 'Springular',
+      description: 'Full Stack Portfolio',
+      deploy: 'fa-google', github: 'fa-github',
+      link_deploy: 'https://springular.web.app', link_repo: 'https://github.com/josuehoenicka/springular',
+      img: '../../../../assets/items/building.png'
+    },
+    {
       name: 'Bandicootfolio',
-      description: 'Portfolio',
-      deploy: 'fa-cloud', github: 'fa-github',
+      description: 'Front End Portfolio',
+      deploy: 'fa-google', github: 'fa-github',
       link_deploy: 'https://bandicootfolio.web.app/', link_repo: 'https://github.com/josuehoenicka/bandicootfolio',
       img: '../../../../assets/items/building.png'
     },
     {
       name: 'Hoenixstore',
       description: 'Ecommerce Plataform',
-      deploy: 'fa-cloud', github: 'fa-github',
+      deploy: 'fa-ban', github: 'fa-github',
       link_deploy: 'https://hoenixstore.web.app/', link_repo: 'https://github.com/josuehoenicka/hoenixstore',
       img: '../../../../assets/items/building.png'
     },
@@ -26,20 +40,6 @@ export class ExperienceComponent {
       description: 'Travel Application',
       deploy: 'fa-ban', github: 'fa-github',
       link_deploy: '', link_repo: 'https://github.com/josuehoenicka/wanderlist',
-      img: '../../../../assets/items/building.png'
-    },
-    {
-      name: 'Springular',
-      description: 'Portfolio',
-      deploy: 'fa-ban', github: 'fa-github',
-      link_deploy: '', link_repo: 'https://github.com/josuehoenicka/springular',
-      img: '../../../../assets/items/building.png'
-    },
-    {
-      name: 'Jonikingames',
-      description: 'Video Game Website',
-      deploy: 'fa-ban', github: 'fa-github',
-      link_deploy: '', link_repo: 'https://github.com/josuehoenicka/jonikingames',
       img: '../../../../assets/items/building.png'
     }
   ]
